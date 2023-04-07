5adf0b2be1f494f528269785d9fd537e6b1fb8f7
884153724@qq.com <570588655@qq.com>
2023-04-07 23:14:18 +0800

    :sparkles: add routes

--- src/router.config.tsx
+++ src/router.config.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home'
+// import Home from './pages/Home'
 import Login from './pages/Login'
+import { lazy } from 'react'
+
+const Home = lazy(() => import('./pages/Home'))
 
 const routes = createBrowserRouter([
   {
